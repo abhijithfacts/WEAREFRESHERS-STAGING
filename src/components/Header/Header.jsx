7a4825887669b90e7a1570bbccdd4da944d36410
Abhijith <abhijithkr.facts@gmail.com>
2025-11-13 10:45:41 +0400

    about page responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,59 +1,82 @@
-import React from "react";
+"use client";
+import React, {useEffect, useState } from "react";
 import styles from "./headerStyles.module.css";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 const Header = () => {
-  return (
-    <div className={styles.headerContainer}>
-      <div className={styles.headerTopSection}>
-        <div className={styles.burgerMenu}>
-          <Image
-            className={styles.burgerImage}
-            alt="nav-burger"
-            src="/icons/burger.png"
-            width={40}
-            height={40}
-            quality={100}
-          />
-        </div>
-        <Link href={"/"} className={styles.linkUnstyled}>
-          <Image
-            className={styles.headerLogo}
-            alt="header-logo"
-            width={200}
-            height={100}
-            src="/images/desktop_nav_logo.png"
-            quality={100}
-          />
-        </Link>
-        <h5 className={styles.userGreet}>Welcome FRESHER USER 123</h5>
-        <div className={styles.mobileAvatar}>
-          <Image
-            className={styles.userIcon}
-            alt="user-icon"
-            width={40}
-            height={40}
-            src="/icons/user.png"
-            quality={100}
-          />
-        </div>
-      </div>
-      <div className={styles.headerNavSection}>
-        <div className={styles.headerNavItems}>
-          <Link href={"/about"} className={styles.linkUnstyled}>
-            <h5 className={styles.navItem}>About</h5>
-          </Link>
-          <Link href={"/overview"} className={styles.linkUnstyled}>
-            <h5 className={styles.navItem}>Overview</h5>
-          </Link>
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const pathname = usePathname();
 
-          <h5 className={styles.navItem}>Categories</h5>
-          <h5 className={styles.navItem}>Special Orders</h5>
-          <h5 className={styles.navItem}>Contact Us</h5>
+  useEffect(() => {
+    setShowMobileMenu(false);
+  }, [pathname]);
+  return (
+    <>
+      <MobileMenu
+        showMobileMenu={showMobileMenu}
+        closeMenu={() => setShowMobileMenu(false)}
+      />
+      <div className={styles.headerContainer}>
+        <div className={styles.headerTopSection}>
+          <div
+            className={styles.burgerMenu}
+            onClick={() => setShowMobileMenu((prev) => !prev)}
+          >
+            <Image
+              className={styles.burgerImage}
+              alt="nav-burger"
+              src="/icons/burger.png"
+              width={40}
+              height={40}
+              quality={100}
+            />
+          </div>
+          <Link href={"/"} className={styles.linkUnstyled}>
+            <Image
+              className={styles.headerLogo}
+              alt="header-logo"
+              width={200}
+              height={100}
+              src="/images/desktop_nav_logo.png"
+              quality={100}
+            />
+          </Link>
+          <h5 className={styles.userGreet}>Welcome FRESHER USER 123</h5>
+          <div className={styles.mobileAvatar}>
+            <Image
+              className={styles.userIcon}
+              alt="user-icon"
+              width={40}
+              height={40}
+              src="/icons/user.png"
+              quality={100}
+            />
+          </div>
+        </div>
+        <div className={styles.headerNavSection}>
+          <div className={styles.headerNavItems}>
+            <Link href={"/about"} className={styles.linkUnstyled}>
+              <h5 className={styles.navItem}>About</h5>
+            </Link>
+            <Link href={"/overview"} className={styles.linkUnstyled}>
+              <h5 className={styles.navItem}>Overview</h5>
+            </Link>
+            <Link href={"/overview"} className={styles.linkUnstyled}>
+              <h5 className={styles.navItem}>Categories</h5>
+            </Link>
+            <Link href={"/overview"} className={styles.linkUnstyled}>
+              <h5 className={styles.navItem}>Special Offer</h5>
+            </Link>
+            <Link href={"/overview"} className={styles.linkUnstyled}>
+              <h5 className={styles.navItem}>Contact Us</h5>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styles from "./Mobilemenu.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const MobileMenu = ({ showMobileMenu,closeMenu }) => {
+  return (
+    <div
+      className={styles.MenuWrap}
+      style={{ display: showMobileMenu ? "flex" : "none" }}
+    >
+      <div className={styles.MenuClose} onClick={closeMenu}>
+        <Image
+          className={styles.CloseIcon}
+          src="/icons/close.png"
+          alt="cart-icon"
+          width={600}
+          height={600}
+        />
+      </div>
+      <div className={styles.GreetSection}>
+        <h6 className={styles.WelcomeText}>Welcome FRESHER USER 123</h6>
+      </div>
+      <div className={styles.CartSection}>
+        <Image
+          className={styles.Icons}
+          src="/icons/cart.png"
+          alt="cart-icon"
+          width={600}
+          height={600}
+        />
+        <Image
+          className={styles.Icons}
+          src="/icons/user.png"
+          alt="cart-icon"
+          width={600}
+          height={600}
+        />
+      </div>
+      <div className={styles.NavSection}>
+        <Link className={styles.NavItems} href="/about">
+          About
+        </Link>
+        <Link className={styles.NavItems} href="/about">
+          Overview
+        </Link>
+        <Link className={styles.NavItems} href="/about">
+          Categories
+        </Link>
+        <Link className={styles.NavItems} href="/about">
+          Special Orders
+        </Link>
+        <Link className={styles.NavItems} href="/about">
+          Contact Us
+        </Link>
+      </div>
+      <div className={styles.SearchSection}>
+        <input className={styles.NavSearch} placeholder="Search" />
+        <Image
+          className={styles.Icons}
+          src="/icons/search.png"
+          alt="cart-icon"
+          width={100}
+          height={100}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
